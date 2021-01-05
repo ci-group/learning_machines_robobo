74e52894c06fa39ab2a023e7435eb1a6c77444b4
sandersiepel <Qnwzcs2qw@@>
2021-01-05 23:28:34 +0100

    expanded to 5 states per sensor,  instead of 2

--- src/main_test.py
+++ src/main_test.py
@@ -10,18 +10,23 @@ import random
 import pprint
 
 
+class Direction:
+    LEFT = (-25, 25, 300)  # Action: 0
+    RIGHT = (25, -25, 300)  # Action: 1
+    FORWARD = (25, 25, 300)  # Action: 2
+    RRIGHT = (50, -50, 300)  # Action: 3
+    LLEFT = (-50, 50, 300)  # Action: 4
+
+
 class Environment:
     ITERATIONS = 10_000
     DURATION_SIMULATION = 250
     LEARNING_RATE = .1
     DISCOUNT_FACTOR = .95
-    EPSILON = .8
+    EPSILON = .6
 
-    action_space = [0, 1, 2]
-    state_space = None
-    observation_space = None  # All possible observations; rob's 8 sensors. Can become quite large, therefore
-    # needs bucketing.
-    collision_boundary = .05
+    action_space = [0, 1, 2, 3, 4]
+    collision_boundary = .1
     collision_counter = 0
 
     def __init__(self):
@@ -36,10 +41,12 @@ class Environment:
 
             while self.valid_environment():
                 curr_state = self.handle_state()
+                print(f"Current state: {curr_state} and its values: {np.round(self.q_table[curr_state], 2)}")
                 if random.random() < (1 - self.EPSILON):
                     best_action = random.choice(self.action_space)
                 else:
                     best_action = np.argmax(self.q_table[curr_state])
+                    print(best_action)
 
                 self.update_q_table(best_action)
             else:
@@ -59,8 +66,8 @@ class Environment:
         # Initialize Q-table for states * action pairs with default values (0).
         # Since observation space is very large, we need to trim it down (bucketing) to only a select amount of
         # possible states, e.g. 4 for each sensor (4^8 = 65k). Or: use less sensors (no rear sensors for task 1).
-        q_table = np.random.uniform(low=0, high=2, size=([2, 2, 2, 2, 2] + [len(self.action_space)]))
-        q_table[(0, 0, 0, 0, 0)] = np.array([0, 0, 1])  # Initialize the table with forward move
+        q_table = np.random.uniform(low=0, high=0, size=([5, 5, 5, 5, 5] + [len(self.action_space)]))
+        q_table[(0, 0, 0, 0, 0)][2] = 1  # Initialize the table with forward move.
         return np.round(q_table)
 
     def handle_state(self):
@@ -69,42 +76,64 @@ class Environment:
         # the bucket size is already determined by the shape of the q_table.
         sensor_values = np.log(np.array(self.rob.read_irs())[3:]) / 10
         sensor_values = np.where(sensor_values == -np.inf, 0, sensor_values)  # Remove the infinite values.
-        sensor_values = (sensor_values - -0.65) / 0.65  # Scales all variables between [0, 1] where 0 is close.
+        sensor_values = (sensor_values - -0.65) / 0.65  # Scales all variables between [0, 1] where 0 is close proximity.
 
         # Check what the actual sensor_values are (between [0, 1]) and determine their state
         indices = []
         for sensor_value in sensor_values:
-            if 1 >= sensor_value >= 0.7:  # No need for action, moving forward is best.
+            if 1 >= sensor_value >= 0.8:  # No need for action, moving forward is best.
                 indices.append(0)
-            else:
-                indices.append(1)  # Action!
+            elif 0.8 > sensor_value >= 0.6:  # No need for action, moving forward is best.
+                indices.append(1)
+            elif 0.6 > sensor_value >= 0.4:  # No need for action, moving forward is best.
+                indices.append(2)
+            elif 0.4 > sensor_value >= 0.2:  # No need for action, moving forward is best.
+                indices.append(3)
+            elif 0.2 > sensor_value >= 0:  # No need for action, moving forward is best.
+                indices.append(4)
 
         return tuple(indices)
 
     def handle_action(self, action):
         # This function should accept an action (0, 1, 2) and move the robot accordingly (left, right, forward).
         # Return: new_state and reward
-        reward = 0
+        reward = -1
 
         # Keep track of collision. If it reaches its threshold, reset environment.
         collision = self.collision()
 
         if action == 0:
-            left, right, duration = -40, 40, 200  # Left
-            if collision:
-                reward += 1
-            else:
-                reward -= 1
+            left, right, duration = Direction.LEFT  # Left, action 0
+            # self.rob.move(-10, -10, 50)
+            # if collision:
+            #     reward += 1
+            # else:
+            #     reward -= 1
         elif action == 1:
-            left, right, duration = 40, -40, 200  # Right
-            if collision:
-                reward += 1
-            else:
-                reward -= 1
+            left, right, duration = Direction.RIGHT  # Right, action 1
+            # self.rob.move(-10, -10, 50)
+            # if collision:
+            #     reward += 1
+            # else:
+            #     reward -= 1
+        elif action == 3:
+            left, right, duration = Direction.RRIGHT  # Extreme right, action 3
+            # self.rob.move(-10, -10, 50)
+            # if collision:
+            #     reward += 1
+            # else:
+            #     reward -= 1
+        elif action == 4:
+            left, right, duration = Direction.LLEFT  # Extreme left, action 4
+            # self.rob.move(-10, -10, 50)
+            # if collision:
+            #     reward += 1
+            # else:
+            #     reward -= 1
         else:
-            left, right, duration = 40, 40, 200  # Forward
+            left, right, duration = Direction.FORWARD  # Forward, action 2
             if collision:
-                reward -= 1
+                reward -= 4
             else:
                 reward += 2
 
@@ -133,8 +162,6 @@ class Environment:
         # Calculate the new Q-value with the common formula
         new_q = (1 - self.LEARNING_RATE) * current_q + self.LEARNING_RATE * (
                 reward + self.DISCOUNT_FACTOR * max_future_q)
-
-        print(f"Current Q: {current_q}, received reward: {reward}, new Q: {new_q}")
 
         self.q_table[new_state][best_action] = new_q  # And update the value
 
