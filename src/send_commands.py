54e81596aa16ebb3ce14b958a28b891547adfc3f
Peter Santos <AIMEPTSTSAM@mail.com>
2021-01-07 13:41:15 +0000

    Change to local host

--- src/send_commands.py
+++ src/send_commands.py
@@ -19,7 +19,7 @@ def main():
     signal.signal(signal.SIGINT, terminate_program)
 
     # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
-    rob = robobo.SimulationRobobo().connect(address='192.168.1.6', port=19997)
+    rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
 
     rob.play_simulation()
 
