fe70e72ae13d6191b39df6873efb42b6c0dea20a
Peter Santos <AIMEPTSTSAM@mail.com>
2021-01-11 23:52:55 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -35,20 +35,16 @@ class Controller:
         self.gene = gene
         self.nn = NN(x, h, y)
         state_dict = self.nn.state_dict()
-        w1 = [[gene[6 * i + j] for j in range(6)] for i in range(10)]
-        b1 = [gene[60 + i] for i in range(10)]
-        w2 = [[gene[70 + 10 * i + j] for j in range(10)] for i in range(2)]
-        b2 = [gene[90 + i] for i in range(2)]
+        w1 = [[gene[x * i + j] for j in range(x)] for i in range(h)]
+        b1 = [gene[x * h + i] for i in range(h)]
+        w2 = [[gene[(x + 1) * h + h * i + j] for j in range(h)] for i in range(y)]
+        b2 = [gene[(x + 1 + y) * h + i] for i in range(y)]
         state_dict['linear1.weight'] = torch.tensor(w1)
         state_dict['linear1.bias'] = torch.tensor(b1)
         state_dict['linear2.weight'] = torch.tensor(w2)
         state_dict['linear2.bias'] = torch.tensor(b2)
         self.nn.load_state_dict(state_dict, strict=False)
-        # self.nn.linear1.weight = torch.tensor(w1)
 
-
-    def construct(self):
-        pass
 
 if __name__ == '__main__':
     gene = [0.1] * 60 + [0.2] * 10 + [0.3] * 20 + [0.4] * 2
