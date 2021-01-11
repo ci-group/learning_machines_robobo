9529a164f083567d575248b7f9b04ad7c69407a1
Peter Santos <AIMEPTSTSAM@mail.com>
2021-01-11 22:46:40 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -18,10 +18,10 @@ num_input = x, num_hidden = h, num_output = y
 linear 1: w1 (h, x); b1 (h)
 linear 2: w2 (y, h); b2 (y)
 gene: 
-    w1: [0 : x*h]
-    b1: [x*h+1 : (x+1)*h]
-    w2: [(x+1)*h+1 : (x+1+y)*h]
-    b2: [(x+1+y)*h+1 : (x+y+1)*h+y]
+    w1: [0 : x*h-1]
+    b1: [x*h : (x+1)*h-1]
+    w2: [(x+1)*h : (x+1+y)*h-1]
+    b2: [(x+1+y)*h : (x+y+1)*h+y-1]
 
 total length: (x+y+1)*h+y
 """
@@ -29,19 +29,28 @@ total length: (x+y+1)*h+y
 
 class Controller:
     def __init__(self, gene=None):
+        x = 6
+        h = 10
+        y = 2
         self.gene = gene
-        self.nn = NN(6, 10, 2)
-        for i in range(10):
-            for j in range(6):
-                self.nn.linear1.weight[i][j] = gene[6 * i + j]
+        self.nn = NN(x, h, y)
+        state_dict = self.nn.state_dict()
+        w1 = [[gene[6 * i + j] for j in range(6)] for i in range(10)]
+        b1 = [gene[60 + i] for i in range(10)]
+        w2 = [[gene[70 + 10 * i + j] for j in range(10)] for i in range(2)]
+        b2 = [gene[90 + i] for i in range(2)]
+        state_dict['linear1.weight'] = torch.tensor(w1)
+        state_dict['linear1.bias'] = torch.tensor(b1)
+        state_dict['linear2.weight'] = torch.tensor(w2)
+        state_dict['linear2.bias'] = torch.tensor(b2)
+        self.nn.load_state_dict(state_dict, strict=False)
+        # self.nn.linear1.weight = torch.tensor(w1)
 
 
     def construct(self):
         pass
 
 if __name__ == '__main__':
-    gene = [0.5] * 92
-    n = NN(6, 10, 2)
+    gene = [0.1] * 60 + [0.2] * 10 + [0.3] * 20 + [0.4] * 2
     c = Controller(gene)
-    print(n.linear1.weight)
     # print(c.nn.linear2.bias)
