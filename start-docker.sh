c141c69942a67eea17dfeb6928e557a85a15092a
Daniel Engbert <end1@umbc.edu>
2022-12-04 14:59:03 +0100

    docker: use host network for simplicity.

--- start-docker.sh
+++ start-docker.sh
@@ -3,4 +3,4 @@
 set -e
 
 PROJECT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/"
-exec docker run --rm -it -v "${PROJECT_FOLDER}:/root/projects/" cigroup/learning-machines:python3 bash
+exec docker run --rm -it -v "${PROJECT_FOLDER}:/root/projects/" --net=host cigroup/learning-machines:python3 bash
