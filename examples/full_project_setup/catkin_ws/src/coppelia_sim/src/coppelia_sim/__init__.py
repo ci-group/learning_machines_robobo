b19228e2276db3db6fa21bd0ecfb302f0f31690d
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-12-28 16:01:19 +0100

    Add CoppeliaSim's remoteAPI so to version control

--- examples/full_project_setup/catkin_ws/src/coppelia_sim/src/coppelia_sim/__init__.py
+++ examples/full_project_setup/catkin_ws/src/coppelia_sim/src/coppelia_sim/__init__.py
@@ -1,3 +1,6 @@
 """CoppeliaSim compatability module
 Taken from https://github.com/mintforpeople/robobo-coppelia-simulator
+
+Mentionned "CoppeliaSim user manual" is here:
+https://www.coppeliarobotics.com/helpFiles/en/apiFunctions.htm
 """
