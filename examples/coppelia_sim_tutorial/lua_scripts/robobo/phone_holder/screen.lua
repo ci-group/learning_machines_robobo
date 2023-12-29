b22059673dc2ebc75c6f5b6cf3aedffecdf41ff5
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-12-29 16:29:11 +0100

    Cleanup lua code

--- examples/coppelia_sim_tutorial/lua_scripts/robobo/phone_holder/screen.lua
+++ examples/coppelia_sim_tutorial/lua_scripts/robobo/phone_holder/screen.lua
@@ -1,43 +1,23 @@
 function sysCall_init()
     -- do some initialization here
+    print("test")
     screen = sim.getObjectHandle("Screen")
+    defaultEmotion = ""
 end
 
 function sysCall_cleanup()
     -- do some clean-up here
-    face, img, resolution = sim.createTexture(
-        "C:/Users/Juanatey/robobo/pictures/normal.jpg",
-        0,
-        nil,
-        nil,
-        nil,
-        0,
-        nil
-    )
+    face, img, resolution = sim.createTexture(defaultEmotion, 0, nil, nil, nil, 0, nil)
     sim.setShapeTexture(screen, img, sim.texturemap_cube, 1100, { 0.13, 0.13 }, nil, nil)
 end
 
-function setEmotionTo(inInts, inFloats, emotion, inBuffer)
-    if emotion[1] == "happy" then
-        ruta = "C:/Users/Juanatey/robobo/pictures/happy.jpg"
-    elseif emotion[1] == "laughing" then
-        ruta = 0
-    elseif emotion[1] == "surprised" then
-        ruta = 0
-    elseif emotion[1] == "sad" then
-        ruta = "C:/Users/Juanatey/robobo/pictures/sad.jpg"
-    elseif emotion[1] == "angry" then
-        ruta = "C:/Users/Juanatey/robobo/pictures/angry.jpg"
-    elseif emotion[1] == "normal" then
-        ruta = "C:/Users/Juanatey/robobo/pictures/normal.jpg"
-    elseif emotion[1] == "sleeping" then
-        ruta = "C:/Users/Juanatey/robobo/pictures/sleeping.jpg"
-    elseif emotion[1] == "tired" then
-        ruta = 0
-    elseif emotion[1] == "afraid" then
-        ruta = 0
-    end
-    face, img, resolution = sim.createTexture(ruta, 0, nil, nil, nil, 0, nil)
+setDefaultEmotion = function(inIntegers, inFloats, inStrings, inBuffer)
+    defaultEmotion = inStrings[1]
+    return {}, {}, {}, ""
+end
+
+setEmotionTo = function(inIntegers, inFloats, inStrings, inBuffer)
+    face, img, resolution = sim.createTexture(inStrings[1], 0, nil, nil, nil, 0, nil)
     sim.setShapeTexture(screen, img, sim.texturemap_cube, 1100, { 0.13, 0.13 }, nil, nil)
     return {}, {}, {}, ""
 end
