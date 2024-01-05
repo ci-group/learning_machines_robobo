a13a590f506ba816dad6722fec5f57497901e7ed
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-01-05 15:05:49 +0100

    Add --rm to docker flag for convenience

--- examples/full_project_setup/scripts/run.ps1
+++ examples/full_project_setup/scripts/run.ps1
@@ -8,4 +8,4 @@ param($p1)
 docker build --tag learning_machines .
 # Mounting to a directory that does not exist creates it.
 # Mounting to relative paths works since docker engine 23
-docker run -t -p 45100:45100 -p 45101:45101 -v ./results:/root/results learning_machines $PSBoundParameters["p1"]
+docker run -t --rm -p 45100:45100 -p 45101:45101 -v ./results:/root/results learning_machines $PSBoundParameters["p1"]
