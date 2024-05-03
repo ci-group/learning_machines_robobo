0a5c1dd447437ddc6d2b7f5cf5fba543307d54a6
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-05-03 14:42:17 +0200

    Increase stability on Windows

--- maintained/scripts/run.ps1
+++ maintained/scripts/run.ps1
@@ -8,7 +8,10 @@ param(
     [String[]] $arguments
 )
 
+# Replace newlines to `\n`, as `\r\n` is still quite unstable with Docker, even for the dockerfile.
+(Get-Content "$(Get-Location)\Dockerfile") -join "`n" | Set-Content "$(Get-Location)\Dockerfile"
+
 docker build --tag learning_machines .
 # Mounting to a directory that does not exist creates it.
 # Mounting to relative path doesn't create non-existing directories on Windows.
-Invoke-Expression -Command "docker run -t --rm -p 45100:45100 -p 45101:45101 -v '${PWD}/results:/root/results' learning_machines $arguments"
+Invoke-Expression -Command "docker run -t --rm -p 45100:45100 -p 45101:45101 -v '$(Get-Location)\results:/root/results' learning_machines $arguments"
--- maintained/scripts/start_coppelia_docker.ps1
+++ maintained/scripts/start_coppelia_docker.ps1
@@ -11,17 +11,18 @@ param(
 # For some reason, "Path.GetTempFileName" exists, creating a new temp file,
 # but there is no "Path.GetTempDirName", so we have to do it ourselves like this.
 $tempDir = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [System.IO.Path]::GetRandomFileName())
-New-Item -Path $tempDir -ItemType Directory | Out-Null
+New-Item -Path "$tempDir" -ItemType Directory | Out-Null
 
 # Copying everything over to a temp dir, and running the docker build there.
 # That way, the Dockerfile alwyas knows where the scene you want to load is, and can copy it in.
-Copy-Item -Path $scenePath -Destination (Join-Path -Path $tempDir -ChildPath "to_open.ttt")
-Copy-Item -Path ".\CoppeliaSim.tar.xz" -Destination $tempDir
-Copy-Item -Path ".\Dockerfile" -Destination $tempDir
+Copy-Item -Path "$scenePath" -Destination (Join-Path -Path "$tempDir" -ChildPath "to_open.ttt")
+Copy-Item -Path "$(Get-Location)\CoppeliaSim.tar.xz" -Destination "$tempDir"
+# Replace newlines to `\n` during copy, as `\r\n` is still quite unstable with Docker, even for the dockerfile.
+(Get-Content "$(Get-Location)\Dockerfile") -join "`n" | Set-Content "$($tempDir)\Dockerfile"
 
-Push-Location -Path $tempDir
+Push-Location -Path "$tempDir"
 docker build -t coppelia_sim .
 Pop-Location
-Remove-Item -Path $tempDir -Recurse -Force
+Remove-Item -Path "$tempDir" -Recurse -Force
 
 Invoke-Expression -Command "docker run -it --rm -p '$($apiPort):$($apiPort)' --name 'coppelia_$($apiPort)' coppelia_sim '-GzmqRemoteApi.rpcPort=$($apiPort)'"
